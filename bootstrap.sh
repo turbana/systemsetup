4da0ae74ab5ec8b929ba4ea07dbb29a59f5fb303
Ian Clark <turbana@gmail.com>
2018-06-11 13:33:38 -0700

    Added git to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,5 +19,5 @@ fi
 apt-get install -y dirmngr
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 apt-get update
-apt-get install -y ansible python
+apt-get install -y ansible python git
 ansible-pull -U $GIT_REPO
