50acb60ef7e4e11dbb194d2f4262f64fdc05ed4c
Ian Clark <turbana@gmail.com>
2018-06-12 15:11:44 -0700

    Update bootstrap.sh with ssh key gen

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
 
-set -ex
+set -e
 
 GIT_REPO=https://github.com/turbana/systemsetup.git
 SOURCES=/etc/apt/sources.list
 TMP_SOURCES=/tmp/$$.sources.list
 
+if [ "$#" = "1" ]; then
+    USER=$1
+else
+    echo " * finding user"
+    USER=$(id -un 1000)
+fi
+if [ -z "$USER" ]; then
+    echo "ERROR: could not determine user; run '$(basename $0) <user>'"
+    exit 2
+fi
+
+echo " * user is $USER"
+
 # add ansible repo
 if ! grep -q ansible $SOURCES; then
+    echo " * add ansible apt repository"
     cp $SOURCES $TMP_SOURCES
     echo >> $TMP_SOURCES
     echo "# ansible repo" >> $TMP_SOURCES
@@ -16,8 +30,26 @@ if ! grep -q ansible $SOURCES; then
 fi
 
 # dirmngr needed for apt-key
-apt-get install -y dirmngr
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-apt-get update
-apt-get install -y ansible python git
+echo " * install dirmngr"
+apt-get install -y dirmngr > /dev/null
+
+echo " * add ansible public apt key"
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367 > /dev/null
+
+echo " * update packages"
+apt-get update > /dev/null
+
+echo " * install ansible/git/sudo"
+apt-get install -y ansible python git sudo > /dev/null
+
+if [ ! -f $HOME/id_ecdsa ]; then
+    echo " * generate ssh key"
+    sudo -u $USER ssh-keygen -f /home/$USER/.ssh/id_ecdsa -t ecdsa -b 521
+    echo " * MANUALLY add the public key to github"
+    cat /home/$USER/.ssh/id_ecdsa.pub
+    echo "Press enter to continue..."
+    read ignore
+fi
+
+echo " * start ansible"
 ansible-pull -U $GIT_REPO
