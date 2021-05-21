16104bece02f90ce2e39af936d8af61e11a62702
Ian Clark <turbana@gmail.com>
2021-05-21 13:32:27 -0700

    Added channels.scm

---
+++ guix/channels.scm
@@ -0,0 +1,13 @@
+(list
+ ;; base guix channel
+ (channel
+  (name 'guix)
+  (url "https://git.savannah.gnu.org/git/guix.git")
+  ;; (commit
+  ;;  "696cf48b98ad4ef03c88cdb5a83c739618c8fa16")
+  (introduction
+   (make-channel-introduction
+    "9edb3f66fd807b096b48283debdcddccfea34bad"
+    (openpgp-fingerprint
+     "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+ )
