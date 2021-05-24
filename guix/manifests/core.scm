a02d8fd4ee61bce74045767c5afc3a0f957c1070
Ian Clark <turbana@gmail.com>
2021-05-24 13:36:31 -0700

    Added python-language-server to core profile

--- guix/manifests/core.scm
+++ guix/manifests/core.scm
@@ -12,6 +12,14 @@
    "make"
    ;; python
    "python"
+   "python-language-server"
+   ;; "python-flake8"
+   ;; "python-jedi"
+   ;; "python-nose"
+   ;; "python-pyflakes"
+   ;; "python-rope"
+   ;; "python-pytest"
+   ;; "python-autopep8"
    ;; misc
    "unzip"
    ))
